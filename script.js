0453a3e92a5ee30af911d3fc0f791da65ffcda3b
John Doe <cwright05@icloud.com>
2023-06-29 17:10:50 -0500

    Made the carousel work for 2

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
+//Make sure that the Title is always lined up with the top of the image carousel
+window.addEventListener('load', function() {
+  adjustTitlePosition();
+});
+
+window.addEventListener('resize', function() {
+  adjustTitlePosition();
+});
+
+function adjustTitlePosition() {
+  //var title = document.querySelector('.title');
+  var paragraphs = document.querySelectorAll('.description');
+  var parentDivs = document.querySelectorAll('.carousel-slide');
+  console.log("The number of paragraphs is " + paragraphs.length)
+  console.log(imageTop + "it");
+  for (let i = 0; i < paragraphs.length; i++){
+    var firstImage = parentDivs[i].querySelector('img');
+    var imageTop = firstImage.offsetTop;
+    paragraphs[i].style.top = (imageTop) + "px";
+  }
+  }
+
+
 //Code for IMAGE carosel as utilized in general projects
+
 // Wait for the page to load
 window.addEventListener('load', function() {
-  var slideContainer = document.querySelector('.carousel-slide');
-  var prevBtn = document.querySelector('.carousel-prev');
-  var nextBtn = document.querySelector('.carousel-next');
+  // Function to initialize a carousel
+  function initializeCarousel(carouselContainer) {
+    var slideContainer = carouselContainer.querySelector('.carousel-slide');
+    var prevBtn = carouselContainer.querySelector('.carousel-prev');
+    var nextBtn = carouselContainer.querySelector('.carousel-next');
 
-  // Initialize carousel
-  var currentIndex = 0; // Store the current slide index
-  var slideWidth = slideContainer.clientWidth; // Get the width of the window and set the slide to that
-  var totalSlides = slideContainer.children.length; // Get the total number of slides ( how many children .carousel-slide had)
+    // Initialize carousel
+    var currentIndex = 0;
+    var slideWidth = slideContainer.parentNode.offsetWidth;
+    var totalSlides = slideContainer.children.length;
 
-  // Move to the next slide
-  function nextSlide() {
-    currentIndex = (currentIndex + 1) % totalSlides; // Calculate the next slide index (wrap around if necessary)
-    slideContainer.style.transform = `translateX(-${currentIndex * slideWidth * .3333333333333}px)`; // Move the slide container horizontally to display the next slide
+    function nextSlide() {
+      slideWidth = slideContainer.parentNode.offsetWidth;
+      currentIndex = (currentIndex + 1) % totalSlides;
+      slideContainer.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+    }
+
+    function prevSlide() {
+      slideWidth = slideContainer.parentNode.offsetWidth;
+      currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+      slideContainer.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+    }
+
+    // Attach event listeners to the buttons
+    nextBtn.addEventListener('click', nextSlide);
+    prevBtn.addEventListener('click', prevSlide);
+
+    // Automatically move to the next slide every 5 seconds
+    setInterval(nextSlide, 3000);
   }
 
-  // Move to the previous slide
-  function prevSlide() {
-    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides; // Calculate the previous slide index (wrap around if necessary)
-    slideContainer.style.transform = `translateX(-${currentIndex * slideWidth * .3333333333333333333}px)`; // Move the slide container horizontally to display the previous slide
-  }
+  // Get all carousel containers
+  var carouselContainers = document.querySelectorAll('.carousel-container');
 
-  // Attach event listeners to the buttons
-  nextBtn.addEventListener('click', nextSlide); // Call the nextSlide() function when the "Next" button is clicked
-  prevBtn.addEventListener('click', prevSlide); // Call the prevSlide() function when the "Prev" button is clicked
-
-
-  // Automatically move to the next slide every 5 seconds
-  setInterval(nextSlide, 5000);
+  // Initialize each carousel
+  carouselContainers.forEach(function(carouselContainer) {
+    initializeCarousel(carouselContainer);
+  });
 });
 
 
@@ -40,7 +74,7 @@ var home = true;
 //add hover fade in and out
 var bar = document.querySelector('.app-bar');
 bar.addEventListener('mouseenter', () => {
-  bar.style.opacity = '1';
+  bar.style.opacity = '.9';
 });
 bar.addEventListener('mouseleave', () => {
   // if you are not on the home screen fadeaway. Therefore if you are on homescreen it will always be present
@@ -60,12 +94,28 @@ logos.forEach(function (logo) {
     //derived from removing _logo
     //Will be used to show the application
     var applicationId = logoId.split('_')[0];
-    //Show what needs to be shown
-    document.getElementById(applicationId).style.display="inline";
-    //hide homescreen image
+    // Hide all divs
+
+  //Hide all apps
+  var apps = document.querySelectorAll(".app");
+  for (var i = 0; i < apps.length; i++) {
+    apps[i].classList.add("hidden");
+  }
+
+  // Show selected App
+  var shownApp = document.getElementById(applicationId);
+  if (shownApp) {
+    console.log("ran");
+    shownApp.classList.remove("hidden");
+  }
+
+
+
     var screen = document.getElementById("homescreen");
     screen.style.backgroundSize = "0 0";
     home = false;
+    //recheck title position
+    adjustTitlePosition();
   });
 });
 
